b6a8c043682eebbae14047a3a4154c8c782cd6e4
Bat.bat <williamjiang237@outlook.com>
2019-12-08 16:46:53 +0800

    Makefile: Fix bugs

--- Makefile.sh
+++ Makefile.sh
@@ -184,6 +184,7 @@ function Install() {
     # Drivers
     for dotEFIdir in "../Clover/Drivers/UEFI" "../OpenCore/OC/Drivers"; do
         cp -R Drivers/*.efi "$dotEFIdir"
+        cp -R ../Shared/UEFI/*.efi "$dotEFIdir"
     done
 
     # ACPI
